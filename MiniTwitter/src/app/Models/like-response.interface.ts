26e8d0e9178756e5e406423cc3dcc25d1321ad61
Maria <mariamaciaspadilla@gmail.com>
2020-11-26 21:13:40 +0100

    algunos cambios

---
+++ MiniTwitter/src/app/Models/like-response.interface.ts
@@ -0,0 +1,8 @@
+export interface Like {
+  id: number;
+  username: string;
+  descripcion: string;
+  website: string;
+  photoUrl: string;
+  created: Date;
+}
--- MiniTwitter/src/app/app.module.ts
+++ MiniTwitter/src/app/app.module.ts
@@ -4,26 +4,34 @@ import { FormsModule } from "@angular/forms"
 import { HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 
+//import { MatCardModule } from '@angular/material/card';
+//import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './Components/login/login.component';
 import { RegistroComponent } from './Components/registro/registro.component';
 import { UsuarioService } from './Services/usuario.service';
+import { ListaTweetsComponent } from './Components/lista-tweets/lista-tweets.component';
+import { TweetsService } from './Services/tweets.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    ListaTweetsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule
+
   ],
   providers: [
     UsuarioService,
+    TweetsService,
     CookieService
   ],
   bootstrap: [AppComponent]
